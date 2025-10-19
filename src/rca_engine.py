ab639cbe8e8db2e55e0b8fba5b8eaa739ec6efbd
Olseenoy <99533109+Olseenoy@users.noreply.github.com>
2025-10-19 13:40:17 +0100

    Update rca_engine.py

--- src/rca_engine.py
+++ src/rca_engine.py
@@ -18,7 +18,7 @@ from langchain_groq import ChatGroq
 from langchain_community.llms import Ollama
 from langchain_community.chat_models import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain_core.chains import LLMChain
 from transformers import pipeline
 from docx import Document
 from pypdf import PdfReader
